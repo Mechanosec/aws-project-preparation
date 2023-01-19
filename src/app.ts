dcbf31b65341309e29d08f9d087bd3a6dc9c722b
o.krasovskyi <mechanosec29@gmail.com>
2023-01-19 15:49:16 +0200

    cloudfront creator

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,10 @@ const run = () => {
   const globalName = 'try.test.lumi';
 
   createS3(globalName).then(() => {
-    createCloudFront(globalName);
+    createCloudFront(
+      globalName,
+      'arn:aws:acm:us-east-1:830658900665:certificate/8d72d961-5da8-4f2c-8f81-3ae166b969a5'
+    );
   });
 };
 
--- src/aws/cloud_front.ts
+++ src/aws/cloud_front.ts
@@ -8,11 +8,14 @@ import { awsConfig } from '../config';
 
 const client = new CloudFrontClient(awsConfig);
 
-const createCloudFront = async (cloudFrontName: string) => {
+const createCloudFront = async (
+  cloudFrontName: string,
+  ACMCertificateArn: string
+) => {
   const distribution = new CreateDistributionCommand({
     DistributionConfig: {
       CallerReference: cloudFrontName,
-      Enabled: false,
+      Enabled: true,
       Origins: {
         Quantity: 1,
         Items: [
@@ -22,10 +25,17 @@ const createCloudFront = async (cloudFrontName: string) => {
             CustomOriginConfig: {
               HTTPPort: 80,
               HTTPSPort: 80,
-              OriginProtocolPolicy: OriginProtocolPolicy.http_only,
+              OriginProtocolPolicy: OriginProtocolPolicy.https_only,
             },
           },
         ],
+      },
+      Aliases: {
+        Quantity: 1,
+        Items: [cloudFrontName],
+      },
+      ViewerCertificate: {
+        ACMCertificateArn,
       },
       DefaultCacheBehavior: {
         TargetOriginId: `${cloudFrontName}.s3-website.-us-east-1.amazonaws.com`,
@@ -44,6 +54,7 @@ const createCloudFront = async (cloudFrontName: string) => {
 
   try {
     await client.send(distribution);
+
     console.log('_____DISTRIBUTION CREATED_____');
   } catch (error) {
     console.log(error);
